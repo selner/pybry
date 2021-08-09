3b3dfceb46b76fa48e7b936510dfdd1940f0b968
Bryan Selner <dev@recoilvelocity.com>
2021-08-08 19:39:08 -0700

    adding base setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+	name='pybry',
+	version='',
+	packages=['pybry'],
+	url='https://github.com/selner/pybry',
+	license='',
+	author='dev@recoilvelocity.com',
+	author_email='dev@recoilvelocity.com',
+	description='',
+	install_requires=[
+		"setuptools>=45.0",
+	])
